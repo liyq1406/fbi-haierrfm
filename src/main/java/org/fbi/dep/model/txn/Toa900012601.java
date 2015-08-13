f41829b72ca92d90803714ec79a41c2248300729
baiqg <baiyang_1212@163.com>
2015-08-13 13:06:17 +0800

    对账交易

--- src/main/java/org/fbi/dep/model/txn/Toa900012601.java
+++ src/main/java/org/fbi/dep/model/txn/Toa900012601.java
@@ -5,14 +5,13 @@ import org.fbi.dep.model.base.TOABody;
 import org.fbi.dep.model.base.TOAHeader;
 
 import java.io.Serializable;
-import java.util.List;
 
 /**
- * Created by hanjianlong on 2015-8-10.
- * 账户当日交易明细查询
+ * 泰安房产资金监管：日终对账信息取得
+ * User: hanjianlong
+ * Date: 2015-07-16
  */
-
-public class Toa900012601 extends TOA {
+public class Toa900012601 extends TOA implements Serializable {
     public Header header = new Header();
     public Body body = new Body();
 
@@ -31,110 +30,9 @@ public class Toa900012601 extends TOA {
     }
 
     public static class Body extends TOABody {
-        /*01	外围系统流水号
-          02	交易金额
-        */
-        public String TOTCNT;       // 总记录数
-        public String CURCNT;       // 本包内记录数
-        public String DBTCNT;       // 借方总笔数
-        public String DBTAMT;       // 借方总金额
-        public String CRTCNT;       // 贷方总笔数
-        public String CRTAMT;       // 贷方总金额
-        public List<BodyDetail> DETAILS;
-        public String RTN_REQ_SN;
-        public String RTN_TX_AMT;
-    }
-
-    public static class BodyDetail implements Serializable {
-        public String CUSIDT;       // 客户号
-        public String APCODE;       // 核算码
-        public String CURCDE;       // 货币号
-        public String STMDAT;       // 交易日期
-        public String TLRNUM;       // 交易柜员
-        public String VCHSET;       // 传票套号
-        public String SETSEQ;       // 传票套内序号
-        public String TXNAMT;       // 交易金额
-        public String LASBAL;       // 交易后余额
-        public String FURINF;       // 摘要
-
-        public String getCUSIDT() {
-            return CUSIDT;
-        }
-
-        public void setCUSIDT(String CUSIDT) {
-            this.CUSIDT = CUSIDT;
-        }
-
-        public String getAPCODE() {
-            return APCODE;
-        }
-
-        public void setAPCODE(String APCODE) {
-            this.APCODE = APCODE;
-        }
-
-        public String getCURCDE() {
-            return CURCDE;
-        }
-
-        public void setCURCDE(String CURCDE) {
-            this.CURCDE = CURCDE;
-        }
-
-        public String getSTMDAT() {
-            return STMDAT;
-        }
-
-        public void setSTMDAT(String STMDAT) {
-            this.STMDAT = STMDAT;
-        }
-
-        public String getTLRNUM() {
-            return TLRNUM;
-        }
-
-        public void setTLRNUM(String TLRNUM) {
-            this.TLRNUM = TLRNUM;
-        }
-
-        public String getVCHSET() {
-            return VCHSET;
-        }
-
-        public void setVCHSET(String VCHSET) {
-            this.VCHSET = VCHSET;
-        }
-
-        public String getSETSEQ() {
-            return SETSEQ;
-        }
-
-        public void setSETSEQ(String SETSEQ) {
-            this.SETSEQ = SETSEQ;
-        }
-
-        public String getTXNAMT() {
-            return TXNAMT;
-        }
-
-        public void setTXNAMT(String TXNAMT) {
-            this.TXNAMT = TXNAMT;
-        }
-
-        public String getLASBAL() {
-            return LASBAL;
-        }
-
-        public void setLASBAL(String LASBAL) {
-            this.LASBAL = LASBAL;
-        }
-
-        public String getFURINF() {
-            return FURINF;
-        }
-
-        public void setFURINF(String FURINF) {
-            this.FURINF = FURINF;
-        }
+        public String DRCNT;   //借方总笔数
+        public String DRAMT;   //借方总金额
+        public String CRCNT;   //保留字段
+        public String CRAMT;   //保留字段
     }
 }
