1c541f7aed1f46f0bb2aa31997e9f4363a1c3266
baiqg <baiyang_1212@163.com>
2015-09-06 18:08:01 +0800

    划拨修改

--- src/main/java/rfm/ta/view/TaPayoutAction.java
+++ src/main/java/rfm/ta/view/TaPayoutAction.java
@@ -233,7 +233,7 @@ public class TaPayoutAction {
 
                     MessageUtil.addInfo(toaSbs.getHeader().RETURN_MSG);
                 } else { // SBS记账失败的处理
-                    taAccDetlService.deleteRecord(taRsAccDtl.getPkId());
+                    taAccDetlService.deleteRecord(taRsAccDtlPara.getPkId());
                     MessageUtil.addInfo(toaSbs.getHeader().RETURN_MSG);
                     return false;
                 }
